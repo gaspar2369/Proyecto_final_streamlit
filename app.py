89a498d23f4cee7e9661c6fb14cac05f07e3135c
gaspar2369 <albertogaspar91@gmail.com>
2026-06-15 16:33:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+# ============================================================
+# Proyecto Final: App Analizadora de Datasets con Streamlit
+# Autor: Fernando Alberto Gaspar Callanaupa
+# Año: 2026
+# ============================================================
+
+# -----------------------------
+# Importando librerías necesarias
+# -----------------------------
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+# -----------------------------
+# Configuración inicial de la App
+# -----------------------------
+st.set_page_config(page_title="App Analizadora de Datasets", layout="wide")
