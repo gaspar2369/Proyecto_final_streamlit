979d69bc6f731aa0cfabaea7295be2de2c80be0f
gaspar2369 <albertogaspar91@gmail.com>
2026-06-15 15:49:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-# ============================================================
-# Proyecto Final: App Analizadora de Datasets con Streamlit
-# Autor: [Tu Nombre Completo]
-# Año: 2026
-# ============================================================
 
-# -----------------------------
-# Librerías necesarias
-# -----------------------------
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
